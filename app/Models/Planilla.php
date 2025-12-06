aa91832b6597bce7bdee8dce7611830ac8f43b11
civiJS <jeiderarmandosolano@gmail.com>
2025-12-06 18:43:06 -0500

    Actualizacion de modelos

--- app/Models/Planilla.php
+++ app/Models/Planilla.php
@@ -8,12 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Planilla extends Model
 {
     protected $table = 'planilla';
-    
-    public $timestamps = false;
-    protected $primaryKey = 'uuid';  // <- le dices que la PK es UUID
-    public $incrementing = false;    // <- no es autoincremental
-    protected $keyType = 'string';   // <- tipo de la llave es string
-    protected $fillable = ['uuid','FechaCreacion','UsuarioAcargo','TipoDeActividad'];
+    protected $primaryKey = 'uuid';
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        'uuid',
+        'fechacreacion',
+        'usuarioacargo',
+        'tipodeactividad',
+    ];
+
 
     public function usuarios()
     {
