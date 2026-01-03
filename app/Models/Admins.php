426b6a874a693bde4bf9bbe7143c3486c812c740
civiJS <jeiderarmandosolano@gmail.com>
2026-01-03 01:55:46 -0500

    correccion de camelcase y pascalcase y errores tipograficos

--- app/Models/Admins.php
+++ app/Models/Admins.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use Illuminate\Foundation\Auth\Admin as Authenticatable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 
